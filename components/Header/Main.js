b446b6c428c432d43babc6a57653a9d0b63693b1
Rashad-Stack <tex.eng.rashed@gmail.com>
2023-05-09 01:31:43 +0600

    Header styling completed

---
+++ components/Header/Main.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import styles from "./styles.module.scss";
+import Link from "next/link";
+import { RiSearch2Line } from "react-icons/ri";
+import { FaOpencart } from "react-icons/fa";
+import { useSelector } from "react-redux";
+
+export default function Main() {
+  const { cart } = useSelector((state) => ({ ...state }));
+  return (
+    <div className={styles.main}>
+      <div className={styles.main_container}>
+        <Link href="/" className={styles.logo}>
+          <img src="/logo.png" alt="" />
+        </Link>
+        <div className={styles.search}>
+          <input type="text" placeholder="Search..." />
+          <div className={styles.search_icon}>
+            <RiSearch2Line />
+          </div>
+        </div>
+
+        <Link href="/cart" className={styles.cart}>
+          <FaOpencart />
+          <span>{cart.length}</span>
+        </Link>
+      </div>
+    </div>
+  );
+}
